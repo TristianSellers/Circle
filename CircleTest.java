1d9adaed8b0d2ac703a7811ddd0f23875d31cb86
Tristian Sellers <tristianesellers@gmail.com>
2023-10-17 11:58:42 -0500

    initialized empty Circle tests

---
+++ CircleTest.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setRadius() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRadius() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCircumference() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getArea() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }
+}
