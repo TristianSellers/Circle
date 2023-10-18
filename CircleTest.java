340129b7a0ff27de2ce9aaf66d7db38468920829
Tristian Sellers <tristianesellers@gmail.com>
2023-10-18 15:33:56 -0500

    added working unit test for .getCircumference() & .getArea()

--- CircleTest.java
+++ CircleTest.java
@@ -1,17 +1,19 @@
+import java.sql.SQLOutput;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CircleTest {
 
     int [] test_numbers = {100, 10, 1, 0 , -1, -10, -100};
+    double [] test_circumference = {628.32, 62.83, 6.28, 0, 0, 0, 0};
+    double [] test_area = {31415.93, 314.16, 3.14, 0, 0, 0, 0};
 
     Circle [] test_circle;
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         test_circle = new Circle [test_numbers.length];
-        for (int i = 0; i < test_numbers.length; i++) { {
+        for (int i = 0; i < test_numbers.length; i++) {
             test_circle[i] = new Circle(test_numbers[i]);
-        }
-
         }
     }
 
@@ -30,11 +32,24 @@ class CircleTest {
 
     @org.junit.jupiter.api.Test
     void getCircumference() {
-//        assert 628.31858 == (getCircumference() && radius == 100.0);
+        for (int i = 0; i < test_circumference.length; i++) {
+            double circumference = test_circle[i].getCircumference();
+            String formattedNumber = String.format("%.2f", circumference);
+            circumference = Double.parseDouble(formattedNumber);
+            assert circumference == test_circumference[i];
+            System.out.println("Circumference[" + i + "] -> " + circumference);
+        }
     }
 
     @org.junit.jupiter.api.Test
     void getArea() {
+        for (int i = 0; i < test_area.length; i++) {
+            double area = test_circle[i].getArea();
+            String formattedNumber = String.format("%.2f", area);
+            area = Double.parseDouble(formattedNumber);
+            assert area == test_area[i];
+            System.out.println("Area[" + i + "] -> " + area);
+        }
     }
 
     @org.junit.jupiter.api.Test
